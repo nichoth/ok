ea9e697d4ec5545d273f5e41bf4a3255f044e249
nichoth <nichoth@gmail.com>
2019-11-13 13:07:06 -0800

    test-browser works

--- test-browser/index.js
+++ test-browser/index.js
@@ -1,6 +1,26 @@
+var ok = require('../')
 var test = require('tape')
+var struct = require('observ-struct')
+var observ = require('observ')
+var { h } = require('preact')
 
-test('example', function (t) {
-    t.pass('wooooooo')
+function View (props) {
+    return <div>view</div>
+}
+
+function sub ({ state, view }) {
+}
+
+var state = struct({
+    hello: observ(null),
+    route: observ(null)
+})
+
+var el = document.body
+
+test('start', function (t) {
+    var { view } = ok(state, View, el)
+    sub({ state, view })
+    t.ok(view, 'returns')
     t.end()
 })
