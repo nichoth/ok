95655f224cca1a729016475beaa78b7305cd543d
nichoth <nichoth@gmail.com>
2019-11-17 12:41:25 -0800

    stuff

--- example/index.js
+++ example/index.js
@@ -28,7 +28,7 @@ function View (props) {
     if (props.route.pathname) var m = router.match(props.route.pathname)
     if (m) var RouteView = m.action(m)
     return <div>
-        <RouteView  {...props} />
+        <RouteView {...props} />
         <hr />
         hello {props.foo + ' '}
         <button onClick={emit(EVENTS.hello.world)}>emit event</button>
--- index.js
+++ index.js
@@ -2,15 +2,13 @@ var { h, render } = require('preact')
 var connect = require('@nichoth/preact-connect')
 var Bus = require('@nichoth/events')
 var catchRoutes = require('@nichoth/catch-routes')
-// var Router = require('ruta3')
-// var observ = require('observ')
 
 function start (state, view, el) {
+    var bus = Bus({ memo: true })
+
     catchRoutes(function (parsedUrl) {
         state.route.set(parsedUrl)
     })
-
-    var bus = Bus({ memo: true })
 
     var _view = connect({ state, bus, view })
     render(h(_view), el)
